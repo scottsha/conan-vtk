5380fa4febe2d59b891e239fb6003738bd27c8fa
scottsha <scottshaloo@gmail.com>
2022-06-23 03:05:09 -0400

    functional on m1 mac

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, CMake, tools
 
 class vtkConan(ConanFile):
     name = "vtk"
-    version = "9.1.a"
-    source_version = "9.1.0"
+    version = "9.1.1.a"
+    source_version = "9.1.1"
     short_version = "9.1"
     homepage = "https://www.vtk.org/"
     git_hash = "285daeedd58eb890cb90d6e907d822eea3d2d092"
@@ -24,37 +24,6 @@ class vtkConan(ConanFile):
     no_copy_source = True
     short_paths = True
     vtk_source_folder = "vtk_source"
-    # required_modules = ["vtkCommonColor"
-    #                     "vtkCommonComputationalGeometry",
-    #                     "vtkCommonCore",
-    #                     "vtkCommonDataModel",
-    #                     "vtkCommonExecutionModel",
-    #                     "vtkCommonMath",
-    #                     "vtkCommonMisc",
-    #                     "vtkCommonSystem",
-    #                     "vtkCommonTransforms",
-    #                     "vtkFiltersCore",
-    #                     "vtkFiltersExtraction",
-    #                     "vtkFiltersGeneral",
-    #                     "vtkFiltersGeometry",
-    #                     "vtkFiltersImaging",
-    #                     "vtkFiltersPoints",
-    #                     "vtkFiltersReebGraph",
-    #                     "vtkFiltersTopology",
-    #                     "vtkInfovisCore",
-    #                     "vtkInfovisBoost",
-    #                     "vtkInfovisBoostGraphAlgorithms",
-    #                     "vtkInfovisLayout",
-    #                     "vtkIOCore",
-    #                     "vtkIOInfovis",
-    #                     "vtkIOMovie",
-    #                     "vtkIOLegacy",
-    #                     "vtkIOGeometry",
-    #                     "vtkIOLegacy",
-    #                     "vtkRenderingCore"
-    #                     "vtkViewsCore",
-    #                     "vtkViewsGeovis",
-    #                     "vtkViewsInfovis"]
 
     def source(self):
         self.run("git clone -b release --single-branch https://gitlab.kitware.com/vtk/vtk.git " + self.vtk_source_folder)
@@ -112,63 +81,14 @@ class vtkConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        # -include /usr/include/c++/11/limits
-        # for vtkModule in self.required_modules:
-        #     cmake.definitions[vtkModule + "_DEFAULT"] = "ON"
-        # cmake.definitions["BUILD_TESTING"] = "OFF"
-        # cmake.definitions["BUILD_EXAMPLES"] = "OFF"
         cmake.definitions["BUILD_SHARED_LIBS"] = "ON " if self.options.shared else "OFF"
-        # if self.options.minimal:
-        #     cmake.definitions["VTK_Group_StandAlone"] = "OFF"
-        #     cmake.definitions["VTK_Group_Rendering"] = "OFF"
-        # if self.options.ioxml:
-        #     cmake.definitions["Module_vtkIOXML"] = "ON"
-        # if self.options.qt:
-        #     cmake.definitions["VTK_Group_Qt"] = "ON"
-        #     cmake.definitions["VTK_QT_VERSION"] = "5"
-        #     cmake.definitions["VTK_BUILD_QT_DESIGNER_PLUGIN"] = "OFF"
-        # if self.options.mpi:
-        #     cmake.definitions["VTK_Group_MPI"] = "ON"
-        #     cmake.definitions["Module_vtkIOParallelXML"] = "ON"
-        # if self.options.mpi_minimal:
-        #     cmake.definitions["Module_vtkIOParallelXML"] = "ON"
-        #     cmake.definitions["Module_vtkParallelMPI"] = "ON"
-        #
-        # if self.settings.build_type == "Debug" and self.settings.compiler == "Visual Studio":
-        #     cmake.definitions["CMAKE_DEBUG_POSTFIX"] = "_d"
-        #
         cmake.configure()
         cmake.build()
         cmake.install()
 
     def package_info(self):
+        # self.cpp_info.libdirs = ["",""]
         self.cpp_info.libs = tools.collect_libs(self)
-
         self.cpp_info.includedirs = [
-            "include/vtk-%s" % self.short_version,
-            "include/vtk-%s/vtknetcdf/include" % self.short_version,
-            "include/vtk-%s/vtknetcdfcpp" % self.short_version
+            "include/vtk-%s" % self.short_version
         ]
-
-    # def package_info(self):
-    #     self.cpp_info.libs = tools.collect_libs(self)
-    #     if self.settings.os == "Linux":
-    #         self.cpp_info.libs += ["dl","pthread"]
-    #     vtk_base_include_dirs = [
-    #         "include/vtk-%s" % self.short_version,
-    #         "include/vtk-%s/vtknetcdf/include" % self.short_version,
-    #         "include/vtk-%s/vtknetcdfcpp" % self.short_version
-    #     ]
-    #     vtk_include_subdirs = vtk_base_include_dirs \
-    #                           + [foo[0] for foo in os.walk(vtk_base_include_dirs[0])]
-    #     self.cpp_info.includedirs = vtk_include_subdirs
-
-
-
-
-    # def package(self):
-    # # Copy the libraries
-    #     if self.options.shared:
-    #         self.copy(pattern="*.dll", dst="bin", keep_path=False)
-    #         self.copy(pattern="*.dylib", dst="lib", keep_path=False)
-    #         self.copy(pattern="*.so*", dst="lib", keep_path=False)
